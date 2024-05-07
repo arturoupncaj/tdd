6e62ed929a50d417fb860f7191f216f59d374a02
Your Name <you@example.com>
2024-05-07 09:01:58 -0500

    first commit

---
+++ TDD.App/Libro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.App
+{
+    public class Libro
+    {
+        public int LibroId { get; set; }
+        public int? SagaId { get;set; }
+    }
+}
---
+++ TDD.Test/LibreriaDescuentoServiceTest.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDD.App;
+
+namespace TDD.Test
+{
+    public class LibreriaDescuentoServiceTest
+    {
+        [Test]
+        public void CalcularDescuentoCaso01()
+        {
+            // calcular de 1 libros
+            var service = new LibreriaDescuentoService();
+            service.AddLibro(new Libro());
+
+
+            var total = service.GetTotal();
+
+            Assert.AreEqual(10, total);
+        }
+
+        [Test]
+        public void CalcularDescuentCaso02()
+        {
+            // calcular de 2 libros diferentes
+            var service = new LibreriaDescuentoService();
+            service.AddLibro(new Libro());
+            service.AddLibro(new Libro());
+
+            var total = service.GetTotal();
+
+            Assert.AreEqual(20, total);
+        }
+
+        [Test]
+        public void CalcularDescuentCaso03()
+        {
+            // calcular de 3 libros diferentes
+            var service = new LibreriaDescuentoService();
+            service.AddLibro(new Libro());
+            service.AddLibro(new Libro());
+            service.AddLibro(new Libro());
+
+            var total = service.GetTotal();
+
+            Assert.AreEqual(30, total);
+        }
+
+        [Test]
+        public void CalcularDescuentCaso04()
+        {
+            // calcular de 2 libros misma saga
+            var service = new LibreriaDescuentoService();
+            service.AddLibro(new Libro { SagaId = 1 });
+            service.AddLibro(new Libro { SagaId = 1 });
+            
+            var total = service.GetTotal();
+
+            Assert.AreEqual(19, total);
+        }
+
+        [Test]
+        public void CalcularDescuentCaso05()
+        {
+            // calcular de 2 libros misma saga
+            var service = new LibreriaDescuentoService();
+            service.AddLibro(new Libro { SagaId = 1 });
+            service.AddLibro(new Libro { SagaId = 2 });
+
+            var total = service.GetTotal();
+
+            Assert.AreEqual(20, total);
+        }
+
+        [Test]
+        public void CalcularDescuentCaso06()
+        {
+            // calcular de 2 libros misma saga
+            var service = new LibreriaDescuentoService();
+            service.AddLibro(new Libro { SagaId = 1 });
+            service.AddLibro(new Libro { SagaId = 1 });
+            service.AddLibro(new Libro { SagaId = 1 });
+
+            var total = service.GetTotal();
+
+            Assert.AreEqual(27, total);
+        }
+
+        [Test]
+        public void CalcularDescuentCaso07()
+        {
+            // calcular de 2 libros misma saga
+            var service = new LibreriaDescuentoService();
+            service.AddLibro(new Libro { SagaId = 1 });
+            service.AddLibro(new Libro { SagaId = 1 });
+            service.AddLibro(new Libro { SagaId = 2 });
+            service.AddLibro(new Libro { SagaId = 2 });
+
+            var total = service.GetTotal();
+
+            Assert.AreEqual(38, total);
+        }
+
+        [Test]
+        public void CalcularDescuentCaso08()
+        {
+            // calcular de 2 libros misma saga
+            var service = new LibreriaDescuentoService();
+            service.AddLibro(new Libro { SagaId = 1, LibroId = 1 });
+            service.AddLibro(new Libro { SagaId = 1, LibroId = 1 });
+            
+            var total = service.GetTotal();
+
+            Assert.AreEqual(20, total);
+        }
+    }
+}
