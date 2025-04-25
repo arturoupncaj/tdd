cc40d12ff2ac887c1b92887e5011de6676c32efb
Anderson Escalante <joelhorna.6821@gmail.com>
2025-04-24 22:40:33 -0500

    ejercicio de prueba

--- TDD.App/Funciones.cs
+++ TDD.App/Funciones.cs
@@ -14,5 +14,22 @@ public class Funciones
         reverse = reverse.Replace(" ", "");
         return reverse.ToLower().Equals(text.ToLower());
     }
-    
+    public int EncontrarMayor(int[] numeros)
+    {
+        if (numeros == null || numeros.Length == 0)
+        {
+            throw new ArgumentException("El arreglo no puede ser nulo o vac�o");
+        }
+
+        int mayor = numeros[0];
+        for (int i = 1; i < numeros.Length; i++)
+        {
+            if (numeros[i] > mayor)
+            {
+                mayor = numeros[i];
+            }
+        }
+        return mayor;
+    }
+
 }
